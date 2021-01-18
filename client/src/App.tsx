5079709eac4c83b4c6545ec9af8ca08ff9977aa5
ivanwang123 <ivanwang02@gmail.com>
2021-01-18 11:14:58 -0500

    Testing landscape controls

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -54,7 +54,7 @@ function App() {
   // if (isError) return <h1>Error: {error}</h1>;
 
   return (
-    <div className="fixed w-full h-full bg-red-100 overflow-y-auto">
+    <div className="portrait:fixed w-full h-full bg-red-100">
       {/* {data.streams.map((stream: any) => { */}
       {/* // console.log(stream.clips[0].embed_url);  */}
       {/* return ( */}
