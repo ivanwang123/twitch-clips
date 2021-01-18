e6dd10614ec8c74e08a5df6df98a3ab7dfbd80dd
ivanwang123 <ivanwang02@gmail.com>
2021-01-18 08:36:24 -0500

    Styled clip info page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,7 @@ import { useSwipeable } from "react-swipeable";
 // }
 
 function App() {
-  // const { isLoading, isError, data, error } = useQuery("streams", fetchStreams);
+  // const { data } = useQuery("streams", fetchStreams);
   // console.log("STREAMS DATA", data);
 
   const handles = useSwipeable({
@@ -38,26 +38,44 @@ function App() {
   // if (isError) return <h1>Error: {error}</h1>;
 
   return (
-    <div className="App w-full h-full bg-red-100">
+    <div className="flex flex-col w-full h-full bg-red-100">
       {/* {data.streams.map((stream: any) => { */}
       {/* // console.log(stream.clips[0].embed_url);  */}
       {/* return ( */}
-      <div className="w-full h-full flex flex-col bg-red-100">
-        <iframe
-          src="https://clips.twitch.tv/embed?clip=SpineySteamyTubersPeanutButterJellyTime&parent=localhost&parent=twitch-clips.vercel.app"
-          title="ACE DA LOUD HJ"
-          height="50%"
-          width="100%"
-          key="SpineySteamyTubersPeanutButterJellyTime"
-          // ref={iframeRef}
-        ></iframe>
-        <div
-          className="w-full flex-grow z-10 bg-gray-500 pointer-events-auto"
-          {...handles}
-        >
-          Swipe
+      {/* <div className="w-full h-full flex flex-col bg-red-100"> */}
+      <iframe
+        src="https://clips.twitch.tv/embed?clip=SpineySteamyTubersPeanutButterJellyTime&parent=localhost&parent=twitch-clips.vercel.app"
+        title="ACE DA LOUD HJ"
+        height="50%"
+        width="100%"
+        key="SpineySteamyTubersPeanutButterJellyTime"
+        // ref={iframeRef}
+      ></iframe>
+      <section
+        className="w-full flex flex-col flex-grow p-3 bg-gray-900"
+        {...handles}
+      >
+        <h1 className="text-3xl text-gray-200 font-bold">RiotGamesBrazil</h1>
+        <span className="block text-gray-500 font-semibold">
+          Playing League of Legends
+        </span>
+        <div className="text-gray-500 font-semibold mt-4">
+          <span className="text-gray-200 font-bold bg-red-600 px-1 mr-2 rounded">
+            Live
+          </span>
+          131026 viewers
         </div>
-      </div>
+        <span className="block text-lg text-gray-200 font-semibold mt-1">
+          CBLOL 2021: 1ª Etapa - Fase de Pontos - Md1 | Semana 1 - Rodada 2
+        </span>
+        <button
+          type="button"
+          className="self-center text-gray-200 font-bold bg-blue-600 px-3 py-2 my-auto rounded"
+        >
+          Watch Now
+        </button>
+      </section>
+      {/* </div> */}
       {/* ); */}
       {/* })} */}
       {/* <iframe
