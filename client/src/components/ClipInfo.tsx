f67ca635af1cb74be209e55d148d79e274e0b40f
ivanwang123 <ivanwang02@gmail.com>
2021-01-29 21:06:49 -0500

    Testing mobile

--- client/src/components/ClipInfo.tsx
+++ client/src/components/ClipInfo.tsx
@@ -23,7 +23,7 @@ function ClipInfo(props: PropTypes) {
   } = props;
 
   return (
-    <div className="flex flex-col w-full min-h-full bg-gray-900 p-3 lg:px-5">
+    <div className="flex flex-col min-h-full bg-gray-900 p-3 lg:px-5">
       <div className="w-full flex text-lg mb-3 landscape:hidden">
         {clipIndex !== 0 && (
           <button
