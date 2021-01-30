0886e1faebd658f30f1cbdb0fb13bf049d8b5a9b
ivanwang123 <ivanwang02@gmail.com>
2021-01-30 10:33:26 -0500

    Add title to navbar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -29,35 +29,37 @@ function NavBar(props: PropTypes) {
         <img className="w-6 h-6" src="res/close.svg" alt="close" />
       </button>
       <section className="w-full flex items-center">
-        <span className="title text-gray-500 text-xl">Zero Andy</span>
+        {/* <div className="flex items-center"> */}
+        <button
+          type="button"
+          className="icon-btn group flex items-center p-1"
+          onClick={() => {
+            toggleLandscapePage(null);
+            refreshClips();
+          }}
+        >
+          <img
+            className="w-6 h-6 transform transition duration-300 group-hover:rotate-90"
+            src="res/refresh.svg"
+            alt="refresh"
+          />
+          <span className="hidden title text-gray-500 text-xl ml-3 lg:block">
+            Zero Andy
+          </span>
+          {/* <span
+              className="hidden text-gray-500 text-sm font-semibold ml-1 transition duration-300 group-hover:text-gray-300
+                        landscape:block lg:ml-2 lg:block"
+            >
+              Refresh
+            </span> */}
+        </button>
+        {/* </div> */}
 
         {/* GAME SELECTION */}
         <span className="flex border-r border-gray-500 ml-auto">
           <GameSelection setGameSelection={setGameSelection} />
         </span>
         <LanguageSelection setLanguageSelection={setLanguageSelection} />
-        <div className="flex items-center ml-6">
-          <button
-            type="button"
-            className="icon-btn group flex items-center p-1"
-            onClick={() => {
-              toggleLandscapePage(null);
-              refreshClips();
-            }}
-          >
-            <img
-              className="w-6 h-6 transform transition duration-300 group-hover:rotate-90"
-              src="res/refresh.svg"
-              alt="refresh"
-            />
-            {/* <span
-              className="hidden text-gray-500 text-sm font-semibold ml-1 transition duration-300 group-hover:text-gray-300
-                        landscape:block lg:ml-2 lg:block"
-            >
-              Refresh
-            </span> */}
-          </button>
-        </div>
       </section>
     </>
   );
